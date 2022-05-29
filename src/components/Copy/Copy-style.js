7ec5246fca5869743b3c8858ed69ea36e64dbbc0
Alvaro <alvarojab@gmail.com>
2022-05-29 11:12:24 +0200

    footer padding

--- src/components/Copy/Copy-style.js
+++ src/components/Copy/Copy-style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Footer2Container = styled.div`
     width:100%;
-    padding:5px;
+    padding:5px 0;
     background:linear-gradient(180deg, #00B1B7 -1.75%, #00686B 81.66%);;
     text-align:center;
     color:white;
