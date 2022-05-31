eab69c122e810af6c070a905f8a7ae9a65bf6edd
unknown <alvarojab@gmail.com>
2022-05-31 10:07:39 +0200

    sticky nav

--- src/containers/Header/Header-style.js
+++ src/containers/Header/Header-style.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
-const HeaderContainer = styled.header`    
+const HeaderContainer = styled.header`  
+    position: -webkit-sticky; 
+    position: sticky;
+    top: 0;
     display: flex;
     justify-content: space-around;
     align-items: center;
@@ -8,7 +11,7 @@ const HeaderContainer = styled.header`
     height: 80px;
     border-bottom: solid 1px black;
     background: linear-gradient(180deg, #00B1B7 -1.75%, #00686B 81.66%);
-    
+    z-index:9;
 `;
 
 export default HeaderContainer;
